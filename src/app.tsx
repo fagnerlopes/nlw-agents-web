b6431e612fe149dbef9a3017cc740eb4697b48fb
Fagner Lopes <fagner.lopes@kinghost.com.br>
2025-07-08 19:37:47 -0300

    Cria as páginas e as rotas de salas

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,16 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
 export function App() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index/>
+          <Route path="/room" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
