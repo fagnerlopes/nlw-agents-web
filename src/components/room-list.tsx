8650b467363a478c4c636e8fcafb1d121f5c3a41
Fagner Lopes <fagner.lopes@kinghost.com.br>
2025-07-11 22:25:37 -0300

    Implementa a tela que exibe a sala e o formulario de perguntas

--- src/components/room-list.tsx
+++ src/components/room-list.tsx
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import { Link } from "react-router-dom";
 import { Badge } from "./ui/badge";
 import { dayjs } from "@/lib/dayjs";
@@ -6,53 +12,61 @@ import { ArrowRight } from "lucide-react";
 import { useRooms } from "@/http/use-rooms";
 
 export function RoomList() {
-  const { data, isLoading } = useRooms()
+  const { data, isLoading } = useRooms();
 
   return (
     <Card>
       <CardHeader>
-        <CardTitle>
-          Salas recentes
-        </CardTitle>
+        <CardTitle>Salas recentes</CardTitle>
         <CardDescription>
           Acesso rápido as salas criadas recentemente
         </CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col gap-3">
         {isLoading && (
-                <div className="flex items-center justify-center h-32">
-                  <span>Carregando...</span>
+          <div className="flex items-center justify-center h-32">
+            <span>Carregando...</span>
+          </div>
+        )}
+        {!data?.length && !isLoading && (
+          <div className="flex items-center justify-center h-32">
+            <span>Nenhuma sala criada ainda</span>
+          </div>
+        )}
+        {data &&
+          data.length > 0 &&
+          data.map((room) => {
+            console.log("CREATED: ", room.createdAt);
+
+            return (
+              <Link
+                key={room.id}
+                className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent"
+                to={`/room/${room.id}`}
+              >
+                <div className="flex-1 flex flex-col gap-1">
+                  <h3 className="font-medium">{room.name}</h3>
+
+                  <div className="flex items-center ga-2">
+                    <Badge variant="secondary" className="font-light text-xs">
+                      {dayjs(room.createdAt)
+                        .fromNow()
+                        .replace("há", "Há")
+                        .replace("em", "Em")}
+                    </Badge>
+                    <Badge variant="secondary" className="font-light text-xs">
+                      {room.questionsCount} pergunta(s)
+                    </Badge>
+                  </div>
                 </div>
-              )}
-              {data?.map(room => {
-                console.log("CREATED: ", room.createdAt)
-
-                return (
-                  <Link
-                    key={room.id} 
-                    className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent"
-                    to={`/rooms/${room.id}`}
-                  >
-                    <div className="flex-1 flex flex-col gap-1">
-                      <h3 className="font-medium">{room.name}</h3>
-
-                      <div className="flex items-center ga-2">
-                        <Badge variant="secondary" className="font-light text-xs">                         
-                          {dayjs(room.createdAt).fromNow().replace('há', 'Há').replace('em', 'Em')}
-                        </Badge>
-                        <Badge variant="secondary" className="font-light text-xs">
-                          {room.questionsCount} pergunta(s)
-                        </Badge>
-                      </div>
-                    </div>
-                    <span className="flex items-center gap-2 text-sm text-muted-foreground">
-                      Entrar
-                      <ArrowRight className="size-3"/>
-                    </span>
-                  </Link>
-                )
-              })}
+                <span className="flex items-center gap-2 text-sm text-muted-foreground">
+                  Entrar
+                  <ArrowRight className="size-3" />
+                </span>
+              </Link>
+            );
+          })}
       </CardContent>
     </Card>
-  )
+  );
 }
