6e713028747012a65c507d508fae94a5a303c3f1
Fagner Lopes <fagner.lopes@kinghost.com.br>
2025-07-12 19:54:15 -0300

    Ajuste na interrupção da gravação de audio

--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -20,14 +20,65 @@ export function RecordRoomAudio() {
 
   const [isRecording, setIsRecording] = useState(false);
   const recorder = useRef<MediaRecorder | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  function createRecorder(stream: MediaStream) {
+    recorder.current = new MediaRecorder(stream, {
+      mimeType: "audio/webm",
+      audioBitsPerSecond: 64_000,
+    });
+
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        uploadAudio(event.data);
+      }
+    };
+
+    recorder.current.onstart = () => {
+      console.log("Gravação iniciada");
+    };
+
+    recorder.current.onstop = () => {
+      console.log("Gravação parada");
+    };
+
+    recorder.current.start();
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported()) {
+      alert("Gravação de áudio não é suportada neste navegador.");
+      return;
+    }
+
+    setIsRecording(true);
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    });
+
+    createRecorder(audio);
+
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop();
+
+      createRecorder(audio);
+    }, 10000);
+  }
 
   function stopRecording() {
+    setIsRecording(false);
+
     if (recorder.current && recorder.current.state !== "inactive") {
       recorder.current.stop();
-      setIsRecording(false);
-    } else {
-      alert("Nenhuma gravação em andamento.");
-      return;
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
     }
   }
 
@@ -47,46 +98,6 @@ export function RecordRoomAudio() {
     const result = await response.json();
 
     console.log("Resposta do servidor:", result);
-  }
-
-  async function startRecording() {
-    if (!isRecordingSupported()) {
-      alert("Gravação de áudio não é suportada neste navegador.");
-      return;
-    }
-
-    setIsRecording(true);
-
-    const audio = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        echoCancellation: true,
-        noiseSuppression: true,
-        sampleRate: 44_100,
-      },
-    });
-
-    recorder.current = new MediaRecorder(audio, {
-      mimeType: "audio/webm",
-      audioBitsPerSecond: 64_000,
-    });
-
-    recorder.current.ondataavailable = (event) => {
-      if (event.data.size > 0) {
-        uploadAudio(event.data);
-        console.log("Dados de áudio gravados:", event.data);
-      }
-    };
-
-    recorder.current.onstart = () => {
-      console.log("Gravação iniciada");
-    };
-
-    recorder.current.onstop = () => {
-      console.log("Gravação parada");
-      setIsRecording(false);
-    };
-
-    recorder.current.start();
   }
 
   if (!params.roomId) {
