c2a7c066eca752de767bc56449767dbb869b3f9e
Fagner Lopes <fagner.lopes@kinghost.com.br>
2025-07-14 11:16:08 -0300

    Implementa lógica de persistência do access token JWT

--- src/http/types/login-response.ts
+++ src/http/types/login-response.ts
@@ -1,4 +1,4 @@
 export type LoginResponse = {
-  bearer_token: string;
+  access_token: string;
   refresh_token?: string;
 };
--- src/http/use-login.ts
+++ src/http/use-login.ts
@@ -1,8 +1,10 @@
 import { useMutation } from "@tanstack/react-query";
 import type { LoginRequest } from "./types/login-request";
 import type { LoginResponse } from "./types/login-response";
+import { useAuthStore } from "../store/use-auth-store";
 
 export function useLogin() {
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
   return useMutation({
     mutationFn: async (data: LoginRequest) => {
       const response = await fetch("http://localhost:3333/login", {
@@ -14,6 +16,11 @@ export function useLogin() {
       });
 
       const result: LoginResponse = await response.json();
+
+      // Corrige: salva o bearer_token no store/localStorage
+      if (result.access_token) {
+        setAccessToken(result.access_token);
+      }
 
       return result;
     },
